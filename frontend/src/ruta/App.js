e4638e7a727aa6e597889d5541140bfda0a303f9
Alexander-quin <66481933+Alexander-quin@users.noreply.github.com>
2020-10-14 17:07:37 -0500

    Cambios #4

--- frontend/src/ruta/App.js
+++ frontend/src/ruta/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 
-import Login from "../src/Paginas/Login";
-import Presentacion from "../src/Paginas/Presentacion";
-import potabi from "../src/Paginas/potabi";
-import Layout from "../src/componentes/Layout";
+
+import Login from "../Paginas/Login";
+import Presentacion from "../Paginas/Presentacion";
+import potabi from "../Paginas/Potabi";
+import Layout from "../componentes/Layout";
 class App extends React.Component {
   render() {
     return (
