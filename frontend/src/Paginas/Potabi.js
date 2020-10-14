9dd089d4e10ac65cfd08f47b778b2260ae3cae71
Alexander-quin <66481933+Alexander-quin@users.noreply.github.com>
2020-10-14 15:49:48 -0500

    cambios #3

---
+++ frontend/src/Paginas/Potabi.js
@@ -0,0 +1,7 @@
+import React from "react";
+import Potabilizacion from "../componentes/potabili";
+
+function Potabi () {
+  return <Potabi />;
+}
+export default Potabilizacion;
--- frontend/src/Paginas/potabi.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import potabi from "../componentes/potabili";
-
-function potabili() {
-  return <potabi />;
-}
-export default potabi;
