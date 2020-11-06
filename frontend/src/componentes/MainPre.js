0b00a6e3d92ae9488b4554a9a8a47563f1b2f278
Alexander Quintana <alesanderamador@gmail.com>
2020-11-06 22:56:39 +0000

    12

--- frontend/src/componentes/MainPre.js
+++ frontend/src/componentes/MainPre.js
@@ -22,7 +22,21 @@ function Presentacion() {
           hacer si no se tiene acceso a agua potable.
         </p>
         <h3 class="nom">Creadores:</h3>
-        
+        <img
+          class="foto"
+          src="https://scontent.feoh1-1.fna.fbcdn.net/v/t1.0-9/118233445_1192079501147540_6682244203532722332_o.jpg?_nc_cat=111&ccb=2&_nc_sid=09cbfe&_nc_eui2=AeGcqnllmKgbzW4VT37Lg9SlKtw8xLmkgOIq3DzEuaSA4roMOO-ZlDOYC-dmS3D6TDyBu1DYIHpXgQdEnfR5XN3d&_nc_ohc=d-JFPugaugUAX_bJ05k&_nc_ht=scontent.feoh1-1.fna&oh=2b5bbdbbf23d12850d30a91eccaa1c0f&oe=5FCBDB99"
+        />
+        <h3 class="nomb">Alexander Quintana</h3>
+        <img
+          class="foto"
+          src="https://scontent.feoh1-1.fna.fbcdn.net/v/t1.0-9/118772526_1585374364957986_6462550181452223908_n.jpg?_nc_cat=102&ccb=2&_nc_sid=174925&_nc_eui2=AeFnl3hGk1DajbTFHbE11jFLHPKV-QpmT_Uc8pX5CmZP9V2U3k76DQ6IMtQ7jYQI7QNWxrajSdvOOXfrn0QZg8aR&_nc_ohc=rIanL03BLYoAX-6bmCF&_nc_ht=scontent.feoh1-1.fna&oh=da4a89072e3fb02fa96d9f9dd529d7db&oe=5FC9DE8D"
+        />
+        <h3  class="nomb1">Mariana Ávila</h3>
+        <img
+          class="foto"
+          src="https://scontent.feoh1-1.fna.fbcdn.net/v/t1.0-9/117186757_2559898750969056_8496950450202372472_o.jpg?_nc_cat=111&ccb=2&_nc_sid=09cbfe&_nc_eui2=AeGqDBELS5RrLlGxHtZa9NU8WKhz8PieM2RYqHPw-J4zZMO-q8bwThDsSVxoZu0cBWI0DRAcLRPJnuCiq34KeXtY&_nc_ohc=2pTxmQT_iVkAX9BSWzn&_nc_ht=scontent.feoh1-1.fna&oh=82f81070de6aa45802a489eaf8f85c60&oe=5FCD230D"
+        />
+        <h3 class="nomb1">Michel Diaz</h3>
       </div>
     </div>
   );
