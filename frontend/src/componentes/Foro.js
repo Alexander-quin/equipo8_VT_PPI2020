c30cacc9dbac94e0ae4422f14da18f8b19638afd
Alexander Quintana <alesanderamador@gmail.com>
2020-11-09 16:02:12 +0000

    img#2

--- frontend/src/componentes/Foro.js
+++ frontend/src/componentes/Foro.js
@@ -42,12 +42,25 @@ function Video() {
       <img src="https://imgv2-2-f.scribdassets.com/img/document/92287189/original/a4eab83dcb/1601117725?v=1" className="imagen2" alt="..."/>
     </div>
     <div className="carousel-item">
-      <img src="https://ecodes.org/images/stories/imagenes2014/espacio3.jpg" className="imagen2" alt="..."/>
+      <img src="https://image.isu.pub/110302150927-5afdd652222c489eb5bc5905d8b5bf64/jpg/page_1.jpg" className="imagen2" alt="..."/>
     </div>
 
     <div className="carousel-item">
       <img src="https://lh3.googleusercontent.com/proxy/Et0e5nPxN-QpTwvXwA6leKmOhrFgcNvW4Wt1-GEMUKFKl9y_U4gCwmzCG98FHPWpBYp7QQd-gDEvhY4lBMNXqiRPga9toQMJasdV1D9lsPI4_hczvx-4FEnwhjXbBdsBZe5cZogNNzsth5RpKFxbEjg0zFAPom50zbs" className="imagen2" alt="..."/>
     </div>
+
+    <div className="carousel-item">
+      <img src="https://img.yumpu.com/32846278/1/500x640/descargar-pdf-publicaciones-caf.jpg" className="imagen2" alt="..."/>
+    </div>
+
+     <div className="carousel-item">
+      <img src="https://images.spot.im/v1/production/sjlb8gu2xyah9hk8vwue" className="imagen2" alt="..."/>
+    </div>
+
+     <div className="carousel-item">
+      <img src="https://www.ins.gov.co/Noticias/SiteAssets/Paginas/Programa-Interlaboratorio-De-Control-De-Calidad-De-Agua-Potable-infografia/ins_instituto-ancional-salud-esquema-agua-potable.jpg" className="imagen2" alt="..."/>
+    </div>
+
   </div>
   <a  className="carousel-control-prev" href="#carouselExampleFade" role="button" data-slide="prev" >
     <span className="carousel-control-prev-icon" aria-hidden="true"></span>
